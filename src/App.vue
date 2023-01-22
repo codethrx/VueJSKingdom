39e2bcaaf7c83af742c653941e9d067c6310b0d6
codethrx <tatheermehdi2000@gmail.com>
2023-01-22 23:23:13 +0500

    More Vue
    Provide and Inject

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <!-- <Basics counter="0" /> -->
+  <h1>{{ val }}</h1>
   <ComputedProps />
 </template>
-<script setup>
+<script>
 import Basics from "./components/Basics.vue";
 import ComputedProps from "./components/ComputedProperties.vue";
+export default {
+  data() {
+    return { val: "Hola" };
+  },
+  components: { Basics, ComputedProps },
+  provide() {
+    return {
+      username: this.val,
+      mail: "abc@gmail.com",
+    };
+  },
+};
 </script>
 <style scoped></style>
