dc33132db06a0f5f0055b81c56c7b901096d7e28
codethrx <tatheermehdi2000@gmail.com>
2023-01-23 00:48:16 +0500

    Components event added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <!-- <Basics counter="0" /> -->
   <h1>{{ val }}</h1>
-  <ComputedProps />
+  <!-- <ComputedProps /> -->
+  <button @click="pop = true">Open</button>
+  <Popup v-show="pop" @close="closePop" />
 </template>
 <script>
 import Basics from "./components/Basics.vue";
+import Popup from "./components/Popup.vue";
 import ComputedProps from "./components/ComputedProperties.vue";
+console.log();
 export default {
   data() {
-    return { val: "Hola" };
+    return { val: "Hola", pop: false };
   },
-  components: { Basics, ComputedProps },
+  components: { Basics, ComputedProps, Popup },
   provide() {
     return {
       username: this.val,
       mail: "abc@gmail.com",
     };
+  },
+  methods: {
+    closePop() {
+      this.pop = false;
+      console.log("Clickd");
+    },
   },
 };
 </script>
---
+++ src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 @click="$emit('close')">Close</h2>
+    <!-- <h2 @click="close">Close</h2> -->
+  </div>
+</template>
+<script>
+// import { emit } from "vue";
+export default {
+  //   props: ["close"],
+  emit: ["close"],
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
